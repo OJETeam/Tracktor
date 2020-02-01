981131d292ad403aceb95c0cdd00c8566df114dc
Eugen1344 <eugen1344@gmail.com>
2020-02-01 13:29:48 +0200

    +avalonia project

---
+++ GUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Logging.Serilog;
+
+namespace GUI
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+        {
+            return AppBuilder.Configure<App>().UsePlatformDetect().LogToDebug();
+        }
+    }
+}
