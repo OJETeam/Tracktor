ab1d13830225703087c71e37c35683c8de0352a2
Eugen1344 <eugen1344@gmail.com>
2020-04-12 18:05:27 +0300

    task window

--- GUI/Windows/MainWindow/MainWindowViewModel.cs
+++ GUI/Windows/MainWindow/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using Avalonia.Controls;
 using Tracktor.Logic;
 
 namespace GUI.Windows.MainWindow
@@ -7,6 +8,8 @@ namespace GUI.Windows.MainWindow
 	public class MainWindowViewModel
 	{
 		public ObservableCollection<Task> Tasks { get; }
+		public GridLength ColumnSize0 { get; set; }
+		public GridLength ColumnSize1 { get; set; }
 
 		public MainWindowViewModel(IEnumerable<Task> tasks)
 		{
