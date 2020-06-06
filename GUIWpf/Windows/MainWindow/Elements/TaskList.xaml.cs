76a6b6b28c93696bdfa5bc8b37bc8cd1d3e67ac8
Eugen1344 <eugen1344@gmail.com>
2020-06-06 13:56:34 +0300

    wpf test

---
+++ GUIWpf/Windows/MainWindow/Elements/TaskList.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUIWpf.Windows.MainWindow.Elements
+{
+	/// <summary>
+	/// Interaction logic for TaskList.xaml
+	/// </summary>
+	public partial class TaskList : Window
+	{
+		public TaskList()
+		{
+			InitializeComponent();
+		}
+	}
+}
